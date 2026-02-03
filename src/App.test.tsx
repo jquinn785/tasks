b055abb9f01f139ee3ae26a91aac59efcc068941
JPWQ <johnpwquinn@proton.me>
2026-02-03 16:03:08 -0500

    Forget to add some files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders the course name somewhere", () => {
-    render(<App />);
-    const linkElement = screen.getByText(/COS420/i);
-    expect(linkElement).toBeInTheDocument();
+  render(<App />);
+  const linkElement = screen.getByText(/COS420/i);
+  expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App(): React.JSX.Element {
-    return (
-        <div className="App">
-            <header className="App-header">
-                UM COS420 with React Hooks and TypeScript
-            </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Hello World
-            </p>
-            <p>
-                John Quinn
-            </p>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        UM COS420 with React Hooks and TypeScript
+      </header>
+      <h1>This is heading text</h1>
+      <img src="mountains.jpg" alt="A picture of the himalaya mountains" />
+      <ol>
+        <li>First thing</li>
+        <li>Another thing</li>
+        <li>A third item</li>
+      </ol>
+      <Button
+        onClick={() => {
+          console.log("Hello World!");
+        }}
+      >
+        Log Hello World
+      </Button>
+      <div
+        style={{ width: "10px", height: "20px", backgroundColor: "red" }}
+      ></div>
+      <p>
+        Edit <code>src/App.tsx</code> and save. This page will automatically
+        reload. Hello World
+      </p>
+      <p>John Quinn</p>
+    </div>
+  );
 }
 
 export default App;
