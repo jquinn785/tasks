3dbb7908a4a9b95ebe853ac0bdab328e9abfb0c8
JPWQ <johnpwquinn@proton.me>
2026-02-28 03:04:07 -0500

    Finished State

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,25 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): React.JSX.Element {
-    return <div>Change Type</div>;
+  const [type, setType] = useState<QuestionType>("short_answer_question");
+
+  function changeType(): void {
+    if (type === "multiple_choice_question") {
+      setType("short_answer_question");
+    } else {
+      setType("multiple_choice_question");
+    }
+  }
+  return (
+    <div>
+      <div>
+        <Button onClick={changeType}>Change Type</Button>
+      </div>
+      {type === "multiple_choice_question" ? (
+        <span>Multiple Choice</span>
+      ) : (
+        <span>Short Answer</span>
+      )}
+    </div>
+  );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -8,9 +8,30 @@ import { Button } from "react-bootstrap";
  * to produce a random integer between 1 and 6 (inclusive).
  */
 export function d6(): number {
-    return 1 + Math.floor(Math.random() * 6);
+  return 1 + Math.floor(Math.random() * 6);
 }
 
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+  const [leftValue, setLeftValue] = useState<number>(5);
+  const [rightValue, setRightValue] = useState<number>(3);
+  function setLeft(): void {
+    setLeftValue(d6());
+  }
+  function setRight(): void {
+    setRightValue(d6());
+  }
+  return (
+    <div>
+      <span data-testid="left-die">{leftValue}</span>
+      <span data-testid="right-die">{rightValue}</span>
+      <Button onClick={setLeft}>Roll Left</Button>
+      <Button onClick={setRight}>Roll Right</Button>
+      {leftValue === 1 && rightValue === 1 ? <span>Lose</span> : <span></span>}
+      {leftValue === rightValue && leftValue != 1 ? (
+        <span>Win</span>
+      ) : (
+        <span></span>
+      )}
+    </div>
+  );
 }
